424fe4f8515c34f8f4a1600d813b9eed1dc2ffb6
keyurbhatiya <keyurbhatiya3004@gamil.com>
2024-12-08 21:52:00 +0530

    day_15

--- Day_15.py
+++ Day_15.py
@@ -2,230 +2,95 @@
 # SciPy
 
 #example
-
-from scipy import constants
-
-print(constants.liter)
-
-# Checking SciPy Version
-
-import scipy
-
-print(scipy.__version__)
-
-# Constants in SciPy
-from scipy import constants
-
-print(constants.pi)
-
-# Constant Units
-from scipy import constants
-
-# print(dir(constants))
-
-# Metric (SI) Prefixes
-
-#example
-
-from scipy import constants
-
-
-print(constants.yotta)    #1e+24
-print(constants.zetta)    #1e+21
-print(constants.exa)      #1e+18
-print(constants.peta)     #1000000000000000.0
-print(constants.tera)     #1000000000000.0
-
-# Binary Prefixes
-
-print(constants.kibi)    #1024
-print(constants.mebi)    #1048576
-print(constants.gibi)    #1073741824
-print(constants.tebi)    #1099511627776
-print(constants.pebi)    #1125899906842624
-print(constants.exbi)    #1152921504606846976
-print(constants.zebi)    #1180591620717411303424
-print(constants.yobi)    #1208925819614629174706176
-
-# Mass
-print(constants.gram)        #0.001
-print(constants.metric_ton)  #1000.0
-print(constants.grain)       #6.479891e-05
-print(constants.lb)          #0.45359236999999997
-print(constants.pound)       #0.45359236999999997
-print(constants.oz)          #0.028349523124999998
-print(constants.ounce)       #0.028349523124999998
-print(constants.stone)       #6.3502931799999995
-
-# Angle
-print(constants.degree)     #0.017453292519943295
-print(constants.arcmin)     #0.0002908882086657216
-print(constants.arcminute)  #0.0002908882086657216
-print(constants.arcsec)     #4.84813681109536e-06
-print(constants.arcsecond)  #4.84813681109536e-06
-
-# Time
-print(constants.minute)      #60.0
-print(constants.hour)        #3600.0
-print(constants.day)         #86400.0
-print(constants.week)        #604800.0
-print(constants.year)        #31536000.0
-print(constants.Julian_year) #31557600.0
-
-
-# Length
-print(constants.inch)              #0.0254
-print(constants.foot)              #0.30479999999999996
-print(constants.yard)              #0.9143999999999999
-print(constants.mile)              #1609.3439999999998
-print(constants.mil)               #2.5399999999999997e-05
-print(constants.pt)                #0.00035277777777777776
-print(constants.point)             #0.00035277777777777776
-print(constants.survey_foot)       #0.3048006096012192
-print(constants.survey_mile)       #1609.3472186944373
-
-# Pressure
-print(constants.atm)         #101325.0
-print(constants.atmosphere)  #101325.0
-print(constants.bar)         #100000.0
-print(constants.torr)        #133.32236842105263
-print(constants.mmHg)        #133.32236842105263
-print(constants.psi)         #6894.757293168361
-
-
-# Optimizers in SciPy
-
+import numpy as np
+from scipy import constants, sparse, io
 from scipy.optimize import root
+from scipy.spatial import Delaunay, ConvexHull
+from scipy.sparse.csgraph import connected_components
+from scipy.interpolate import interp1d
+from scipy.stats import ttest_ind
+import matplotlib.pyplot as plt
 from math import cos
 
-def eqn(x):
-  return x + cos(x)
+# SciPy Constants
+print(f"Liter: {constants.liter}")
+print(f"Pi: {constants.pi}")
 
-# myroot = root(eqn, 0)
+# Metric Prefixes
+metric_prefixes = [constants.yotta, constants.zetta, constants.exa, constants.peta, constants.tera]
+print("Metric Prefixes:", metric_prefixes)
 
-# print(myroot.x)
+# Binary Prefixes
+binary_prefixes = [constants.kibi, constants.mebi, constants.gibi, constants.tebi]
+print("Binary Prefixes:", binary_prefixes)
 
+# Mass Units
+mass_units = [constants.gram, constants.metric_ton, constants.lb, constants.oz]
+print("Mass Units:", mass_units)
 
-# SciPy Sparse Data
+# Angle Units
+angle_units = [constants.degree, constants.arcmin, constants.arcsecond]
+print("Angle Units:", angle_units)
 
-from scipy import sparse
+# Time Units
+time_units = [constants.minute, constants.hour, constants.day, constants.year]
+print("Time Units:", time_units)
 
+# Length Units
+length_units = [constants.inch, constants.foot, constants.mile, constants.point]
+print("Length Units:", length_units)
+
+# Pressure Units
+pressure_units = [constants.atm, constants.bar, constants.torr, constants.psi]
+print("Pressure Units:", pressure_units)
+
+# Optimization Example
+def eqn(x): return x + cos(x)
+root_result = root(eqn, 0)
+print(f"Root of equation: {root_result.x}")
+
+# Sparse Data Example
 A = sparse.csr_matrix([[1, 2, 0], [0, 0, 3], [4, 0, 5]])
-
-print(A)
-
+print("Sparse Matrix:\n", A)
 
 # Connected Components
-import numpy as np
-from scipy.sparse.csgraph import connected_components
-from scipy.sparse import csr_matrix
+arr = np.array([[0, 1, 2], [1, 0, 0], [2, 0, 0]])
+newarr = sparse.csr_matrix(arr)
+n_components, labels = connected_components(newarr)
+print(f"Connected Components: {n_components}, Labels: {labels}")
 
-arr = np.array([
-  [0, 1, 2],
-  [1, 0, 0],
-  [2, 0, 0]
-])
-
-newarr = csr_matrix(arr)
-
-print(connected_components(newarr))
-
-
-# SciPy Spatial Data
-
-import numpy as np
-from scipy.spatial import Delaunay
-import matplotlib.pyplot as plt
-
-points = np.array([
-  [2, 4],
-  [3, 4],
-  [3, 0],
-  [2, 2],
-  [4, 1]
-])
-
+# Delaunay Triangulation
+points = np.array([[2, 4], [3, 4], [3, 0], [2, 2], [4, 1]])
 simplices = Delaunay(points).simplices
-
 plt.triplot(points[:, 0], points[:, 1], simplices)
 plt.scatter(points[:, 0], points[:, 1], color='r')
-
+plt.title("Delaunay Triangulation")
 plt.show()
 
 # Convex Hull
-import numpy as np
-from scipy.spatial import ConvexHull
-import matplotlib.pyplot as plt
-
-points = np.array([
-  [2, 4],
-  [3, 4],
-  [3, 0],
-  [2, 2],
-  [4, 1],
-  [1, 2],
-  [5, 0],
-  [3, 1],
-  [1, 2],
-  [0, 2]
-])
-
+points = np.array([[2, 4], [3, 4], [3, 0], [2, 2], [4, 1], [1, 2], [5, 0], [3, 1]])
 hull = ConvexHull(points)
-hull_points = hull.simplices
-
-plt.scatter(points[:,0], points[:,1])
-for simplex in hull_points:
-  plt.plot(points[simplex,0], points[simplex,1], 'k-')
-
+plt.scatter(points[:, 0], points[:, 1])
+for simplex in hull.simplices:
+    plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+plt.title("Convex Hull")
 plt.show()
 
-
-# SciPy Matlab Arrays
-from scipy import io
-import numpy as np
-
+# MATLAB Arrays Example
 arr = np.arange(10)
-
 io.savemat('arr.mat', {"vec": arr})
-
-# Import Data from Matlab Format
-from scipy import io
-import numpy as np
-
-arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9,])
-
-# Export:
-io.savemat('arr.mat', {"vec": arr})
-
-# Import:
 mydata = io.loadmat('arr.mat')
+print("MATLAB Data Imported:", mydata)
 
-print(mydata)
-
-# SciPy Interpolation
-# 1D Interpolation
-from scipy.interpolate import interp1d
-import numpy as np
-
-xs = np.arange(10)
-ys = 2*xs + 1
-
+# Interpolation Example
+xs, ys = np.arange(10), 2 * np.arange(10) + 1
 interp_func = interp1d(xs, ys)
+new_values = interp_func(np.arange(2.1, 3, 0.1))
+print("Interpolated Values:", new_values)
 
-newarr = interp_func(np.arange(2.1, 3, 0.1))
-
-print(newarr)
-
-# SciPy Statistical Significance Tests
-# T-Test
-import numpy as np
-from scipy.stats import ttest_ind
-
-v1 = np.random.normal(size=100)
-v2 = np.random.normal(size=100)
-
-res = ttest_ind(v1, v2)
-
-print(res)
+# Statistical Significance Test
+v1, v2 = np.random.normal(size=100), np.random.normal(size=100)
+t_test_result = ttest_ind(v1, v2)
+print("T-Test Result:", t_test_result)
 
 ''' Next Topic Django Tutorial ---> Day_16 '''
