751390507a8cb4daa31bad1090617f45d3210010
keyurbhatiya <keyurbhatiya3004@gamil.com>
2025-01-17 20:30:17 +0530

    day_52

--- Day-49-50-51/flappybird.py
+++ Day-49-50-51/flappybird.py
@@ -26,9 +26,7 @@ Step 7: We will last add functions for game over and reset the game.
 import pygame               # importing the pygame module  
 from pygame.locals import * # importing everything from the pygame.locals module  
 import random               # importing the random module  
-
-
-# Scrolling background
+  
 # using the init() function to initialize the pygame window  
 pygame.init()  
   
@@ -39,8 +37,8 @@ game_clock = pygame.time.Clock()
 game_fps = 60  
   
 # defining the width and height of the game screen  
-SCREEN_WIDTH = 750  
-SCREEN_HEIGHT = 900  
+SCREEN_WIDTH = 600  
+SCREEN_HEIGHT = 735  
   
 # using the set_mode() function of the pygame.display module to set the size of the screen  
 display_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
@@ -48,48 +46,48 @@ display_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # setting the title of the application using the set_caption() function  
 pygame.display.set_caption('Flappy Bird - JAVATPOINT')  
   
+# defining the font style  
+fontStyle = pygame.font.SysFont('arial black', 55)  
+  
+# defining the font color  
+black = (0, 0, 0)  
+  
 # declaring and initializing the game variables  
 baseScroll = 0  
 scrollSpeed = 4  
+birdFlying = False  
+gameOver = False  
+pipeGap = 150  
+pipeFrequency = 1450 # milliseconds  
+lastPipe = pygame.time.get_ticks() - pipeFrequency  
+playerScore = 0  
+passPipe = False  
   
 # loading images  
 background = pygame.image.load('images/background.png')  
 base = pygame.image.load('images/base.png')  
+button = pygame.image.load('images/restart.png')  
   
-# declaring a variable and initializing its value with True  
-game_run = True  
+# defining a function to draw the text on the screen  
+def drawText(text, fontStyle, textColor, x_coordinate, y_coordinate):  
+    # using the render() function to render the text as image  
+    image = fontStyle.render(text, True, textColor)  
   
-# using the while loop  
-while game_run:  
-    # setting the fps of the game  
-    game_clock.tick(game_fps)  
+    # using the blit() function to display the image on the screen  
+    display_screen.blit(image, (x_coordinate, y_coordinate))  
   
-    # drawing the background  
-    display_screen.blit(background, (0, 0))  
+# defining a function to reset the game  
+def resetGame():  
+    # calling the empty() function to remove all the sprites  
+    pipeGroup.empty()  
+    # describing the coordinates for the rectangle placement  
+    bird.rect.x = 200  
+    bird.rect.y = int(SCREEN_HEIGHT / 2)  
+    # setting the player score to 0  
+    playerScore = 0  
+    # returning the score  
+    return playerScore  
   
-    # drawing and scrolling the base  
-    display_screen.blit(base, (baseScroll, 768))  
-    baseScroll -= scrollSpeed  
-    if abs(baseScroll) > 70:  
-        baseScroll = 0  
-  
-    # using the for loop to iterate through the events of the game  
-    for event in pygame.event.get():  
-        # setting the variable value to False if the event's type is equivalent to pygame's QUIT constant  
-        if event.type == pygame.QUIT:  
-            game_run = False  
-  
-    # using the update() function of the pygame.display module to update the events of the game  
-    pygame.display.update()  
-  
-# using the quit() function to quit the game  
-pygame.quit()  
-
-
-
-# Day 50: 69 Days of Python
-
-
 # creating a class of the pygame's Sprite() class to display the bird  
 class FlappyBird(pygame.sprite.Sprite):  
     # defining an initializing function  
@@ -114,92 +112,11 @@ class FlappyBird(pygame.sprite.Sprite):
         # setting the current image  
         self.image = self.image_list[self.index]  
           
-        # creating a rectangle to place the bird image      
+        # creating a rectangle to place the bird image  
         self.rect = self.image.get_rect()  
   
         # setting the position of the bird  
         self.rect.center = [x_coordinate, y_coordinate]  
-      
-    # defining a function to handle the animation  
-    def update(self):  
-  
-        # updating the counter by 1  
-        self.counter += 1  
-        # defining a variable to display the sprite cooldown  
-        flapCooldown = 5  
-  
-        # if the counter value is greater than the cooldown  
-        # value set the counter value to 0  
-        if self.counter > flapCooldown:  
-            self.counter = 0  
-              
-            # updating the index value by 1  
-            self.index += 1  
-  
-            # if the index value is greater than or equal to the  
-            # length of the list, set the index value to 0  
-            if self.index >= len(self.image_list):  
-                self.index = 0  
-  
-        # updating the current image  
-        self.image = self.image_list[self.index]  
-          
-# creating an object of the Group() class of the pygame.sprite module  
-birdGroup = pygame.sprite.Group()  
-  
-# creating an object of the FlappyBird() class with  
-bird = FlappyBird(200, int(SCREEN_HEIGHT / 2))  
-  
-# using the add() function to add the object of the FlappyBird() class to the group  
-birdGroup.add(bird)  
-
-
-# using the while loop  
-while game_run:  
-  
-    # setting the fps of the game  
-    game_clock.tick(game_fps)  
-  
-    # drawing the background  
-    display_screen.blit(background, (0, 0))  
-  
-    # drawing the bird  
-    birdGroup.draw(display_screen)  
-  
-    # calling the update() function  
-    birdGroup.update()  
-  
-    # drawing and scrolling the base  
-    display_screen.blit(base, (baseScroll, 768))  
-    baseScroll -= scrollSpeed  
-    if abs(baseScroll) > 70:  
-        baseScroll = 0  
-  
-    # using the for loop to iterate through the events of the game  
-    for event in pygame.event.get():  
-        # setting the variable value to False if the event's type is equivalent to pygame's QUIT constant  
-        if event.type == pygame.QUIT:  
-            game_run = False  
-  
-    # using the update() function of the pygame.display module to update the events of the game  
-    pygame.display.update() 
-
-
-
-# declaring and initializing the game variables  
-birdFlying = False  
-gameOver = False  
-
-# creating a class of the pygame's Sprite() class to display the bird  
-class FlappyBird(pygame.sprite.Sprite):  
-    # defining an initializing function  
-    def __init__(self, x_coordinate, y_coordinate):  
-        pygame.sprite.Sprite.__init__(self)  
-          
-        # code to initialize variables for sprite bird animation  
-        # ...  
-        # ...  
-        # ...  
   
         # defining the initial velocity of the bird  
         self.velocity = 0  
@@ -236,10 +153,26 @@ class FlappyBird(pygame.sprite.Sprite):
                 # setting the pressed variable value to False  
                 self.pressed = False  
   
-            # ...  
-            # ...  
-            # ...  
-            # the sprite animation's update() function code  
+            # updating the counter by 1  
+            self.counter += 1  
+            # defining a variable to display the sprite cooldown  
+            flapCooldown = 5  
+  
+            # if the counter value is greater than the cooldown  
+            # value set the counter value to 0  
+            if self.counter > flapCooldown:  
+                self.counter = 0  
+                  
+                # updating the index value by 1  
+                self.index += 1  
+  
+                # if the index value is greater than or equal to the  
+                # length of the list, set the index value to 0  
+                if self.index >= len(self.image_list):  
+                    self.index = 0  
+  
+            # updating the current image  
+            self.image = self.image_list[self.index]  
               
             # rotating the bird  
             self.image = pygame.transform.rotate(self.image_list[self.index], self.velocity * -2)  
@@ -247,57 +180,7 @@ class FlappyBird(pygame.sprite.Sprite):
         else:  
             # rotating the bird to -90  
             self.image = pygame.transform.rotate(self.image_list[self.index], -90)  
-            # using the while loop  
-while game_run:  
-    # setting the fps of the game  
-    game_clock.tick(game_fps)  
   
-    # drawing the background  
-    display_screen.blit(background, (0, 0))  
-  
-    # drawing the bird  
-    birdGroup.draw(display_screen)  
-  
-    # calling the update() function  
-    birdGroup.update()  
-  
-    # drawing the base  
-    display_screen.blit(base, (baseScroll, 576))  
-  
-    # checking if bird has hit the ground  
-    if bird.rect.bottom > 576:  
-        gameOver = True  
-        birdFlying = False  
-  
-    # checking if the game is not over  
-    if gameOver == False:  
-        # scrolling the base  
-        baseScroll -= scrollSpeed  
-        if abs(baseScroll) > 70:  
-            baseScroll = 0  
-  
-    # using the for loop to iterate through the events of the game  
-    for event in pygame.event.get():  
-        # setting the variable value to False if the event's type is equivalent to pygame's QUIT constant  
-        if event.type == pygame.QUIT:  
-            game_run = False  
-        # setting the variable value to True if the event's type is equivalent to pygame's MOUSEBUTTONDOWN constant, the bird is not flying and game is not over  
-        if event.type == pygame.MOUSEBUTTONDOWN and birdFlying == False and gameOver == False:  
-            birdFlying = True  
-  
-    # using the update() function of the pygame.display module to update the events of the game  
-    pygame.display.update()  
-
-
-
-''' Adding Scrolling Pipes to the Game -> Day 51: 69 Days of Python '''
-
-
-# declaring and initializing the game variables  
-pipeGap = 150  
-pipeFrequency = 1450 # milliseconds  
-lastPipe = pygame.time.get_ticks() - pipeFrequency  
-
 # creating a class of the pygame's Sprite() class to display the pipes  
 class Pipe(pygame.sprite.Sprite):  
     # defining an initializing function  
@@ -326,10 +209,50 @@ class Pipe(pygame.sprite.Sprite):
         if self.rect.right < 0:  
             self.kill()  
   
-# creating an object of the Group() class of the pygame.sprite module  
+# defining a class to display the button  
+class Button():  
+    # defining an initializing function  
+    def __init__(self, x_coordinate, y_coordinate, image):  
+        # defining some variables  
+        self.image = image  
+        self.rect = self.image.get_rect()  
+        self.rect.topleft = (x_coordinate, y_coordinate)  
+    # defining a function to draw the image on the screen  
+    def draw(self):  
+        # setting the initial action to false  
+        action = False  
+  
+        # getting mouse position  
+        position = pygame.mouse.get_pos()  
+  
+        # checking if mouse is over the button  
+        if self.rect.collidepoint(position):  
+            if pygame.mouse.get_pressed()[0] == 1:  
+                action = True  
+                  
+        # drawing button  
+        display_screen.blit(self.image, (self.rect.x, self.rect.y))  
+  
+        # returning the action  
+        return action  
+
+
+# creating the objects of the Group() class of the pygame.sprite module  
+birdGroup = pygame.sprite.Group()  
 pipeGroup = pygame.sprite.Group()  
-
-
+  
+# creating an object of the FlappyBird() class with  
+bird = FlappyBird(200, int(SCREEN_HEIGHT / 2))  
+  
+# using the add() function to add the object of the FlappyBird() class to the group  
+birdGroup.add(bird)  
+  
+# creating the restart button instance  
+restartButton = Button(150, 100, button)  
+  
+# declaring a variable and initializing its value with True  
+game_run = True  
+  
 # using the while loop  
 while game_run:  
     # setting the fps of the game  
@@ -349,6 +272,25 @@ while game_run:
   
     # drawing the base  
     display_screen.blit(base, (baseScroll, 576))  
+  
+    # checking the score  
+    if len(pipeGroup) > 0:  
+        # checking if the bird is over the pipe and passed the left side of it but not the right size  
+        if birdGroup.sprites()[0].rect.left > pipeGroup.sprites()[0].rect.left and birdGroup.sprites()[0].rect.left < pipeGroup.sprites()[0].rect.right and passPipe == False:  
+                # setting the boolean value to true  
+                passPipe = True  
+          
+        # checking if the bird has passed the left side of the pipe  
+        if passPipe == True:  
+            # checking if the bird has passed the right side of the pipe  
+            if birdGroup.sprites()[0].rect.left > pipeGroup.sprites()[0].rect.right:  
+                # incrementing the score by 1  
+                playerScore += 1  
+                # setting the boolean value back to false  
+                passPipe = False  
+  
+    # calling the drawText() function to display the calculated score on the screen  
+    drawText(str(playerScore), fontStyle, black, int(SCREEN_WIDTH / 2), 15)  
   
     # looking for collision  
     if pygame.sprite.groupcollide(birdGroup, pipeGroup, False, False) or bird.rect.top < 0:  
@@ -376,8 +318,15 @@ while game_run:
         baseScroll -= scrollSpeed  
         if abs(baseScroll) > 70:  
             baseScroll = 0  
+              
         # calling the update() function  
         pipeGroup.update()  
+  
+    # checking if the game over and reset  
+    if gameOver == True:  
+        if restartButton.draw() == True:  
+            gameOver = False  
+            playerScore = resetGame()  
   
     # using the for loop to iterate through the events of the game  
     for event in pygame.event.get():  
@@ -390,6 +339,6 @@ while game_run:
   
     # using the update() function of the pygame.display module to update the events of the game  
     pygame.display.update()  
-
-
-'''NEXT TOPIC: Creating a Score Counter in the Game -> Day 52: 69 Days of Python'''
+  
+# using the quit() function to quit the game  
+pygame.quit()  
